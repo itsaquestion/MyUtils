12a26956615ff48069840eac535b0d47eed2ad42
itsaquestion <itsaquestion@gmail.com>
2018-03-29 21:00:42 +0800

    add round_df

---
+++ R/round_df.R
@@ -0,0 +1,19 @@
+#' round_df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' round_df
+round_df <- function(df, digits=2) {
+  mutate_if(df, is.numeric, round, digits=digits)
+}
+
+#' round.data.frame 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' round.data.frame
+round.data.frame = round_df
