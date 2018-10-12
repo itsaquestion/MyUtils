dd0aea575e13dda7fefae0564ee0f67dfd8f9a4f
itsaquestion <itsaquestion@gmail.com>
2018-10-12 16:00:03 +0800

    add debugPrint

---
+++ R/debugPrint.R
@@ -0,0 +1,18 @@
+#.debug_mode = F
+#' @export
+debugPrint = function(...) {
+	if (.debug_mode) {
+		cat("DEBUG > ", ..., "\n")
+	}
+}
+
+#' @export
+debugPrintFun = function() {
+	# print the caller of this function()
+	debugPrint(getCaller(4))
+}
+
+#' @export
+getCaller = function(level = 1) {
+	deparse(sys.calls()[[sys.nframe() - level]])
+}
